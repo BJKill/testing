53974947dba1a6aa3cc25a7ca40ccfa22c46ea6a
BJKill <brandon.j.kill@gmail.com>
2020-05-08 14:12:09 -0400

    added three folders, moved Testing.R into the Scripts folder, and added a message to the end.

--- Scripts/Testing.R
+++ Scripts/Testing.R
@@ -3,3 +3,7 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 print("And I added to the file when I switched computers")
+
+print("And now I added three folders to the testing directory as an organizational example")
+
+print("Yay me?")
